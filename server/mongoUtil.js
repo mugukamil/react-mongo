e2bd20c40d2b0bec0b7b2fdf736f68520e23bbb8
mugukamil <mugukamil@gmail.com>
2017-06-23 02:51:38 +0300

    update

--- server/mongoUtil.js
+++ server/mongoUtil.js
@@ -8,9 +8,15 @@ module.exports = {
     },
 
     connect() {
+<<<<<<< HEAD
         client.connect('mongodb://heroku_jclzrvx9:oj56cvno34isckig6dotsfluup@ds111622.mlab.com:11622/heroku_jclzrvx9', (err, db) => {
             if (err) {
                 console.log(`Error connecting to mongo ${err}`)
+=======
+        client.connect('mongodb://localhost:27017/react-comments', (err, db) => {
+            if (err) {
+                console.log(`Error connecting to mongo ${err}`)
+>>>>>>> 5615817c9e6732254d7d9c613ad1c62ebc29bee9
                 process.exit(1)
             }
 
@@ -23,4 +29,8 @@ module.exports = {
     comments() {
         return _db.collection('comments')
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 5615817c9e6732254d7d9c613ad1c62ebc29bee9
